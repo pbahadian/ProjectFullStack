8b66d4c5b387f74ac118bf5ea23c6b063e482afc
pbahadian <46781071+pbahadian@users.noreply.github.com>
2026-07-22 13:59:24 +0200

    Correct port assignment in server.js
    Fix port assignment to use environment variable correctly.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = require ('./app.js')
 // const PORT = 5888 || 3888 || 5018; SE QUITA Y SE PONE EN .env POR ESP PONEMOS LA LNEA DE ABAJO
 
 
-const port = process.env.PORT || 3888 || 5018
+const port = process.env.PORT 5888 || 3888 || 5018
 
 app.listen(port, () => {
   console.log(`escuchando en el puerto ${port}`);
